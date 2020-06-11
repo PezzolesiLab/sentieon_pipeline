a1534ea8856005afa9f758382df5b1d1296833a5
Scott Frodsham <scott.frodsham@hsc.utah.edu>
2020-06-11 15:20:46 -0600

    not sure what changed, kinda works tho

--- runSentieonPL.sh
+++ runSentieonPL.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-#SBATCH --time=5-00:00:00
+#SBATCH --time=3-00:00:00
 #SBATCH --nodes=1
 #SBATCH -o pipelineKickOff-%j.out
 #SBATCH -e pipelineKickOff-%j.err
 #SBATCH --mail-user=scott.frodsham@hsc.utah.edu
 #SBATCH --mail-type=END
-#SBATCH --account=pezzolesi-np
-#SBATCH --partition=pezzolesi-shared-np
-##SBATCH --partition=ember
+#SBATCH --account=pezzolesi
+#SBATCH --partition=notchpeak-shared
 #SBATCH --ntasks=6
 #SBATCH --mem=32G
 
 # this vvvv should be either 'resume' or 'new'
 resume=$1
+configFile=$2
 # rename this vvvv to create a new directory for your project (i.e. if you want to start over without deleting what you've already done)
-scratchDir=$scr/joslin_wes_jointCalling
+scratchDir=$scr/bckgrndJoslinTargSeq
 #var=$(cat nextflow.config | grep "scratch =")--|
 #IFS="\"" read -ra varArr <<< $var              |- cant get to work yet
 #scratchDir=${varArr[1]}                      --|
@@ -23,27 +23,54 @@ scratchDir=$scr/joslin_wes_jointCalling
 # IGNORE: SLURM_CLUSTERS="notchpeak"
 # IGNORE: export SLURM_CLUSTERS
 
-if [[ $resume == "resume" ]]; then
-    if [ -d $scratchDir ]; then
-        echo -e "\nResuming your previous run\n"
-        cp ./sentieon.nf ./nextflow.config $scratchDir
-        #cp -r ./bin $scratchDir
-        cd $scratchDir
-        nextflow run -with-report -with-trace -with-timeline -with-dag dag.html sentieon.nf -resume
+if [ -z $2 ]; then
+    if [[ $resume == "resume" ]]; then
+        if [ -d $scratchDir ]; then
+            echo -e "\nResuming your previous run\n"
+            cp ./sentieon.nf ./nextflow.config $scratchDir
+            #cp -r ./bin $scratchDir
+            cd $scratchDir
+            nextflow run -with-report -with-trace -with-timeline -with-dag dag.html sentieon.nf -resume
+        else
+            echo "There's nothing to resume (scratch directory doesn't exist)"
+        fi
+    elif [[ $resume == "new" ]]; then
+        if [ ! -d $scratchDir ]; then
+            echo -e "\nStarting a new project\n"
+            mkdir -p $scratchDir/{results/{fastp,fastqc,bqsr,bam/{stats,coverage},gvcf,vcf/stats},bin}
+            cp ./sentieon.nf ./nextflow.config $scratchDir
+            cp -r ./bin $scratchDir
+            cd $scratchDir
+            nextflow run -with-report -with-trace -with-timeline -with-dag dag.html sentieon.nf
+        else
+            echo "A project already exists. Delete it and start over or resume it"
+        fi
     else
-        echo "There's nothing to resume (scratch directory doesn't exist)"
-    fi
-elif [[ $resume == "new" ]]; then
-    if [ ! -d $scratchDir ]; then
-        echo -e "\nStarting a new project\n"
-        mkdir -p $scratchDir/{results/{fastp,fastqc,bqsr,bam/{stats,coverage},gvcf,vcf/stats},bin}
-        cp ./sentieon.nf ./nextflow.config $scratchDir
-        cp -r ./bin $scratchDir
-        cd $scratchDir
-        nextflow run -with-report -with-trace -with-timeline -with-dag dag.html sentieon.nf
-    else
-        echo "A project already exists. Delete it and start over or resume it"
+        echo "Takes string argument 'resume' or 'new'"
     fi
 else
-    echo "Takes string argument 'resume' or 'new'"
+    if [[ $resume == "resume" ]]; then
+        if [ -d $scratchDir ]; then
+            echo -e "\nResuming your previous run\n"
+            cp ./sentieon.nf $configFile $scratchDir
+            #cp -r ./bin $scratchDir
+            cd $scratchDir
+            nextflow -C $configFile run -with-report -with-trace -with-timeline -with-dag dag.html sentieon.nf -resume
+        else
+            echo "There's nothing to resume (scratch directory doesn't exist)"
+        fi
+    elif [[ $resume == "new" ]]; then
+        if [ ! -d $scratchDir ]; then
+            echo -e "\nStarting a new project\n"
+            mkdir -p $scratchDir/{results/{fastp,fastqc,bqsr,bam/{stats,coverage},gvcf,vcf/stats},bin}
+            cp ./sentieon.nf $configFile $scratchDir
+            cp -r ./bin $scratchDir
+            cd $scratchDir
+            nextflow -C $configFile  run -with-report -with-trace -with-timeline -with-dag dag.html sentieon.nf
+        else
+            echo "A project already exists. Delete it and start over or resume it"
+        fi
+    else
+        echo "Takes string argument 'resume' or 'new'"
+    fi
 fi
